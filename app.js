a62fe9aa3358418eb2a71584f728072e17c72c0b
nizzie-2204 <nguyenanhtuan.220499@gmail.com>
2021-12-08 11:54:52 +0700

    update

--- app.js
+++ app.js
@@ -57,13 +57,8 @@ app.use(errorHandler)
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'))
-
-	app.get('*', function (_, res) {
-		res.sendFile(path.join(__dirname, 'index.html'), function (err) {
-			if (err) {
-				res.status(500).send(err)
-			}
-		})
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 	})
 }
 
